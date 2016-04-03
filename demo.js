701156b1c8ff0d69b51784e4fca61f4c4454ffa6
Permagate <kend654@gmail.com>
2016-04-03 14:26:26 +0700

    [Feature] Add basic get command and fix buffering responses

--- demo.js
+++ demo.js
@@ -1,24 +1,13 @@
 'use strict';
+const util = require('util');
 const vndb = require('./index');
 
-// Procedural Style
+const _print = obj => {
+  console.log(util.inspect(obj, false, null));
+};
+
 const client1 = vndb.createClient();
-client1.login().then(null, err => console.log(err));
-client1.dbstats().then(data => console.log(data), err => console.log(err));
+client1.login().then(null, err => _print(err));
+client1.dbstats().then(data => _print(data), err => _print(err));
+client1.get('vn', ['basic', 'details'], 'id >= 1', { results: 3 }).then(data => _print(data), err => _print(err));
 client1.finish();
-
-// Promise Style 1
-const client2 = vndb.createClient();
-client2.login()
-  .then(null, err => console.log(err))
-  .then(() => client2.dbstats())
-  .then(data => console.log(data), err => console.log(err))
-  .then(() => client2.finish());
-
-// Promise Style 2
-const client3 = vndb.createClient();
-client3.login()
-  .then(null, err => console.log(err))
-  .then(client3.dbstats.bind(client3))
-  .then(data => console.log(data), err => console.log(err))
-  .then(client3.finish.bind(client3));

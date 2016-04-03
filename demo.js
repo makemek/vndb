ca6a7ffca00d81e209d1efb9fecd22a5ba7ac123
Permagate <kend654@gmail.com>
2016-04-03 14:59:06 +0700

    [Code] Add more examples

--- demo.js
+++ demo.js
@@ -10,4 +10,7 @@ const client1 = vndb.createClient();
 client1.login().then(null, err => _print(err));
 client1.dbstats().then(data => _print(data), err => _print(err));
 client1.get('vn', ['basic', 'details'], 'id >= 1', { results: 3 }).then(data => _print(data), err => _print(err));
+client1.get('release', ['basic', 'details'], 'id >= 1', { results: 3 }).then(data => _print(data), err => _print(err));
+client1.get('producer', ['basic', 'details'], 'id >= 1', { results: 3 }).then(data => _print(data), err => _print(err));
+client1.get('character', ['basic', 'details'], 'id >= 1', { results: 3 }).then(data => _print(data), err => _print(err));
 client1.finish();

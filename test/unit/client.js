3b58c9390fb787507cca9d508023fab9b4171af7
Permagate <kend654@gmail.com>
2016-11-02 22:08:42 +0700

    Add a new signature (username, password, config) when instantiating a VNDB client

--- test/unit/client.js
+++ test/unit/client.js
@@ -20,6 +20,24 @@ describe('VNDBClient', () => {
         expect(client.config.host).to.equal('test.org');
       });
     });
+
+    describe('with more than 1 argument', () => {
+      beforeEach(function() {
+        this.client = new VNDBClient('testuser', 'testpassword', { host: 'test.org' });
+      });
+
+      it('should override the username using first argument', function() {
+        expect(this.client.config.username).to.equal('testuser');
+      });
+
+      it('should override the password using second argument', function() {
+        expect(this.client.config.password).to.equal('testpassword');
+      });
+
+      it('should override other configs using third argument', function() {
+        expect(this.client.config.host).to.equal('test.org');
+      });
+    });
   });
 });
 

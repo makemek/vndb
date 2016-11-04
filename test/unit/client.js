6c91ff1f2ca7757b72f9a43c14bcc756639807e4
Permagate <kend654@gmail.com>
2016-11-04 19:18:26 +0700

    Rewrite the client again...
    Basically stub everything to plan ahead exactly what I'm going to need.
    But keep (and skip) the test for the old .process method for reference.

--- test/unit/client.js
+++ test/unit/client.js
@@ -5,64 +5,24 @@ const { defaults } = require('../../lib/constants');
 
 describe('VNDBClient', () => {
   describe('constructor', () => {
-    it('should set internal properties', () => {
-      const client = new VNDBClient();
-
-      expect(client._states).to.be.an('object');
-      expect(client._defaults).to.deep.equal(defaults);
-      expect(client.state).to.equal(client._states.new);
-      expect(client.queues).to.be.an('array').that.is.empty;
-      expect(client.socket).not.to.exist;
-    });
-
-    describe.skip('without any argument', () => {
-      it('should be configured with a copy of default values', () => {
-        const client = new VNDBClient();
-
-        expect(client.config).to.not.equal(defaults);
-        expect(client.config).to.deep.equal(defaults);
-      });
-    });
-
-    describe.skip('with 1 argument', () => {
-      it('should be used to override default config', () => {
-        const client = new VNDBClient({ host: 'test.org' });
-
-        expect(client.config.host).to.equal('test.org');
-      });
-    });
-
-    describe.skip('with more than 1 argument', () => {
-      beforeEach(function() {
-        this.client = new VNDBClient('testuser', 'testpassword', { host: 'test.org' });
-      });
-
-      it('should override the username using first argument', function() {
-        expect(this.client.config.username).to.equal('testuser');
-      });
-
-      it('should override the password using second argument', function() {
-        expect(this.client.config.password).to.equal('testpassword');
-      });
-
-      it('should override other configs using third argument', function() {
-        expect(this.client.config.host).to.equal('test.org');
-      });
-    });
-
-    it.skip('should call tls.connect and attach the function result to .socket', function() {
-      this.sandbox.stub(tls, 'connect').returns('a socket');
+    beforeEach(() => {
       this.client = new VNDBClient();
+    });
 
-      expect(this.client.socket).to.equal('a socket');
-      expect(tls.connect).to.have.been.calledWith({
-        host: defaults.host,
-        port: defaults.port,
-      });
+    it('should set default values from constants file', () => {
+      expect(this.client._defaults).to.deep.equal(defaults);
+    });
+
+    it('should initialize an empty queue', () => {
+      expect(this.client.queues).to.be.an('array').that.is.empty;
+    });
+
+    it('should initialize an empty socket', () => {
+      expect(this.client.socket).not.to.exist;
     });
   });
 
-  describe('.process(message)', () => {
+  describe.skip('.process(message)', () => {
     beforeEach(function() {
       this.client = new VNDBClient();
     });

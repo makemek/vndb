b434af76f2e15c91ac5b62a01f53f66bb72773ea
Permagate <kend654@gmail.com>
2016-11-04 19:27:29 +0700

    Rename lib/client to lib/vndb-client and complete the potential test scenarios.

--- test/unit/client.js
+++ test/unit/client.js
@@ -1,9 +1,18 @@
 const { expect } = require('chai');
 const tls = require('tls');
-const VNDBClient = require('../../lib/client');
+const VNDBClient = require('../../lib/vndb-client');
 const { defaults } = require('../../lib/constants');
 
 describe('VNDBClient', () => {
+  describe('._normalizeGetFlags(flags, defaultFlags)', () => {
+  });
+
+  describe('._normalizeGetFilters(filters, defaultFilters)', () => {
+  });
+
+  describe('._normalizeGetOptions(options, defaultOptions)', () => {
+  });
+
   describe('constructor', () => {
     beforeEach(() => {
       this.client = new VNDBClient();
@@ -19,6 +28,98 @@ describe('VNDBClient', () => {
 
     it('should initialize an empty socket', () => {
       expect(this.client.socket).not.to.exist;
+    });
+  });
+
+  describe('.write(message)', () => {
+  });
+
+  describe('.exec(message)', () => {
+  });
+
+  describe('.connect', () => {
+    describe('()', () => {
+    });
+
+    describe('(username, password, config)', () => {
+    });
+  });
+
+  describe('.end()', () => {
+  });
+
+  describe('.dbstats()', () => {
+  });
+
+  describe('.get', () => {
+    describe('(type)', () => {
+    });
+
+    describe('(type, flags, filters, options)', () => {
+    });
+  });
+
+  describe('.vn', () => {
+    describe('()', () => {
+    });
+
+    describe('(flags, filters, options)', () => {
+    });
+  });
+
+  describe('.release', () => {
+    describe('()', () => {
+    });
+
+    describe('(flags, filters, options)', () => {
+    });
+  });
+
+  describe('.producer', () => {
+    describe('()', () => {
+    });
+
+    describe('(flags, filters, options)', () => {
+    });
+  });
+
+  describe('.character', () => {
+    describe('()', () => {
+    });
+
+    describe('(flags, filters, options)', () => {
+    });
+  });
+
+  describe('.user', () => {
+    describe('()', () => {
+    });
+
+    describe('(flags, filters, options)', () => {
+    });
+  });
+
+  describe('.votelist', () => {
+    describe('()', () => {
+    });
+
+    describe('(flags, filters, options)', () => {
+    });
+  });
+
+  describe('.vnlist', () => {
+    describe('()', () => {
+    });
+
+    describe('(flags, filters, options)', () => {
+    });
+  });
+
+  describe('.wishlist', () => {
+    describe('()', () => {
+    });
+
+    describe('(flags, filters, options)', () => {
     });
   });
 

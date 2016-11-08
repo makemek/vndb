2e9863d83c7ad7ef3632b9e800d8841c9f5ddbe5
Permagate <kend654@gmail.com>
2016-11-08 20:18:09 +0700

    Refactor test to follow a certain pattern
    1. Set up conditions/mocks
    2. Execute tested function
    3. Check expectations
    
    Previous step 1 (and sometimes 2) are sometimes done on beforeEach.
    beforeEach block should only be used to set up stubs. If to run
    repeatable commands, wrap it under a function that can be called on each
    it tests.

--- test/unit/vndb-client.js
+++ test/unit/vndb-client.js
@@ -87,6 +87,10 @@ describe('VNDBClient', function() {
   });
 
   describe('.write(message)', function() {
+    beforeEach(function() {
+      this.stubSocket();
+    });
+
     describe('when client.socket is not initialized yet', function() {
       it('should throw an error', function() {
         this.client.socket = null;
@@ -98,7 +102,6 @@ describe('VNDBClient', function() {
 
     describe('when client.bufferedResponse is not null', function() {
       it('should throw an error', function() {
-        this.client.socket = new EventEmitter();
         this.client.bufferedResponse = 'in-progre';
 
         expect(this.client.write.bind(this.client, 'a message'))
@@ -106,70 +109,82 @@ describe('VNDBClient', function() {
       });
     });
 
-    beforeEach(function() {
-      this.stubSocket();
-      this.promise = this.client.write('a message');
+    describe('before receiving "data" event', function() {
+      it('should call client.socket.write with same arg + terminator', function () {
+        this.client.write('a message');
+
+        expect(this.client.socket.write).to.have.been.calledWith(`a message${terminator}`);
+      });
+
+      it('should set client.bufferedResponse to an empty string', function () {
+        this.client.write('a message');
+
+        expect(this.client.bufferedResponse).to.equal('');
+      });
+
+      it('should register a handler to "data" event', function () {
+        this.client.write('a message');
+
+        expect(this.client.socket.listeners('data')).to.have.lengthOf(1);
+      });
     });
 
-    it('should call client.socket.write with same arg + terminator', function() {
-      expect(this.client.socket.write).to.have.been.calledWith(`a message${terminator}`);
-    });
-
-    it('should set client.bufferedResponse to an empty string', function() {
-      expect(this.client.bufferedResponse).to.equal('');
-    });
-
-    it('should register a handler to "data" event', function() {
-      expect(this.client.socket.listeners('data')).to.have.lengthOf(1);
-    });
-
-    describe('on handling "data" event', function() {
-      describe('when receiving a response that does not end with terminator character', function() {
-        it('should save the response in client.bufferedResponse in utf8 format', function() {
+    describe('when receiving "data" event', function() {
+      describe('and it does not end with terminator character', function() {
+        it('should save the response in client.bufferedResponse', function() {
+          this.client.write('a message');
           this.client.bufferedResponse = 'waiting for ';
           this.client.socket.emit('data', Buffer.from('an unfinished response'));
+
           expect(this.client.bufferedResponse).to.equal('waiting for an unfinished response');
         });
       });
 
-      describe('when receiving a response that ends with terminator character', function() {
-        function testCommonBehavior() {
-          it('should set client.bufferedResponse back to null', function() {
-            expect(this.client.bufferedResponse).to.equal(null);
-          });
-
-          it('should deregister the "data" event handler', function() {
-            expect(this.client.socket.listeners('data')).to.be.empty;
-          });
-        }
-
+      describe('and it ends with terminator character', function() {
         describe('and the final response does not begin with "error"', function() {
-          beforeEach(function() {
+          it('should resolve final response without the terminator', function* () {
+            const promise = this.client.write('a message');
             this.client.bufferedResponse = 'waiting for ';
             this.client.socket.emit('data', Buffer.from(`nothing${terminator}`));
+
+            const result = yield promise;
+
+            expect(result).to.equal('waiting for nothing');
           });
 
-          testCommonBehavior();
+          it('should cleanup properly', function* () {
+            const promise = this.client.write('a message');
+            this.client.bufferedResponse = 'waiting for ';
+            this.client.socket.emit('data', Buffer.from(`nothing${terminator}`));
 
-          it('should resolve final response as utf8 string without the terminator', function* () {
-            const result = yield this.promise;
-            expect(result).to.equal('waiting for nothing');
+            yield promise;
+
+            expect(this.client.bufferedResponse).to.equal(null);
+            expect(this.client.socket.listeners('data')).to.be.empty;
           });
         });
 
         describe('and the final response begins with "error"', function() {
-          beforeEach(function() {
+          it('should rejects the final response as a VNDBError object', function* () {
+            const promise = this.client.write('a message');
             this.client.bufferedResponse = 'error {"id": "parse", "msg": "parse er';
             this.client.socket.emit('data', Buffer.from(`ror"}${terminator}`));
-            this.promise.catch(e => e);
+
+            const error = yield this.catchError(promise);
+
+            expect(error).to.be.an.instanceof(VNDBError)
+              .with.property('message', 'parse error');
           });
 
-          testCommonBehavior();
+          it('should cleanup properly', function* () {
+            const promise = this.client.write('a message');
+            this.client.bufferedResponse = 'error {"id": "parse", "msg": "parse er';
+            this.client.socket.emit('data', Buffer.from(`ror"}${terminator}`));
 
-          it('should rejects the final response as a VNDBError object', function* () {
-            const error = yield this.catchError(this.promise);
+            yield this.catchError(promise);
 
-            expect(error).to.be.an.instanceof(VNDBError);
+            expect(this.client.bufferedResponse).to.equal(null);
+            expect(this.client.socket.listeners('data')).to.be.empty;
           });
         });
       });
@@ -178,26 +193,25 @@ describe('VNDBClient', function() {
 
   describe('.exec', function() {
     beforeEach(function() {
-      this.client.bufferedResponse = null;
+      // A helper to generate this.client.queues randomly,
+      // since it expects its items in certain structure.
+      this.generateQueue = (count = 1) => {
+        return Array(count).fill().map((_, i) => {
+          return {
+            message: `Message ${i}`,
+            promise: utils.createDeferredPromise(),
+          };
+        });
+      };
+
       this.stubSocket();
       this.stubWrite();
     });
 
-    // A helper to generate this.client.queues randomly,
-    // since it expects its items in certain structure.
-    const generateQueue = (count = 1) => {
-      return Array(count).fill().map((_, i) => {
-        return {
-          message: `Message ${i}`,
-          promise: utils.createDeferredPromise(),
-        };
-      });
-    };
-
     describe('()', function() {
       describe('when client queues is empty', function() {
         beforeEach(function() {
-          this.client.queues = generateQueue(0);
+          this.client.queues = this.generateQueue(0);
         });
 
         it('should resolve undefined', function* () {
@@ -206,25 +220,29 @@ describe('VNDBClient', function() {
           expect(result).to.equal(undefined);
         });
 
-        it('should not call .write', function() {
-          this.client.exec();
+        it('should not call .write', function* () {
+          yield this.client.exec();
+
           expect(this.client.write).not.to.have.been.called;
         });
       });
 
       describe('when client queues is not empty', function() {
         beforeEach(function() {
-          this.client.queues = generateQueue(1);
-          this.itemToExec = this.client.queues[0];
+          this.client.queues = this.generateQueue(1);
         });
 
         it('should return the first item promise in client queues', function() {
-          const result = this.client.exec();
-          expect(result).to.equal(this.itemToExec.promise);
+          const firstPromise = this.client.queues[0].promise;
+
+          const execPromise = this.client.exec();
+
+          expect(execPromise).to.equal(firstPromise);
         });
 
         it('should call .write', function() {
           this.client.exec();
+
           expect(this.client.write).to.have.been.called;
         });
       });
@@ -233,17 +251,18 @@ describe('VNDBClient', function() {
     describe('(message)', function() {
       it('should call .write', function() {
         this.client.exec('a message');
+
         expect(this.client.write).to.have.been.called;
       });
 
       describe('when client queues is not empty', function() {
         beforeEach(function() {
-          this.client.queues = generateQueue(2);
-          this.itemToExec = this.client.queues[0];
+          this.client.queues = this.generateQueue(2);
         });
 
         it('should queue the message into the last of client queues', function() {
           this.client.exec('a message');
+
           expect(this.client.queues[this.client.queues.length - 1]).to.have.property('message', 'a message');
         });
 
@@ -253,68 +272,82 @@ describe('VNDBClient', function() {
         });
 
         it('should call .write with the first item message in queue', function() {
+          const firstMessage = this.client.queues[0].message;
+
           this.client.exec('a message');
-          expect(this.client.write).to.have.been.calledWith(this.itemToExec.message);
+
+          expect(this.client.write).to.have.been.calledWith(firstMessage);
         });
 
         it('should return the first item promise in client queues', function() {
-          const result = this.client.exec();
-          expect(result).to.equal(this.itemToExec.promise);
+          const firstPromise = this.client.queues[0].promise;
+
+          const execPromise = this.client.exec();
+
+          expect(execPromise).to.equal(firstPromise);
         });
       });
     });
 
     describe('all args', function() {
       beforeEach(function() {
-        this.client.queues = generateQueue(1);
-        this.itemToExec = this.client.queues[0];
+        this.client.queues = this.generateQueue(1);
       });
 
-      describe('on executing message', function() {
-        describe('when client is idle', function() {
-          beforeEach(function() {
-            this.client.bufferedResponse = null;
-          });
-
+      describe('when executing message', function() {
+        describe('and client is idle', function() {
           it('should remove the first item in client queues', function() {
+            const firstMessage = this.client.queues[0].message;
+
             this.client.exec();
+
             expect(this.client.queues)
-              .not.to.contain.an.item.with.property('message', this.itemToExec.message);
+              .not.to.contain.an.item.with.property('message', firstMessage);
           });
 
           it('should execute the first item message in client queues', function() {
+            const firstMessage = this.client.queues[0].message;
+
             this.client.exec();
-            expect(this.client.write).to.have.been.calledWith(this.itemToExec.message);
+
+            expect(this.client.write).to.have.been.calledWith(firstMessage);
           });
         });
 
         function testLazyExec() {
           it('should not remove the first item in client queues', function() {
+            const firstMessage = this.client.queues[0].message;
+
             this.client.exec();
+
             expect(this.client.queues)
-              .to.contain.an.item.with.property('message', this.itemToExec.message);
+              .to.contain.an.item.with.property('message', firstMessage);
           });
 
           it('should not wite any message', function() {
             this.client.exec();
+
             expect(this.client.write).not.to.have.been.called;
           });
 
           it('should still return a promise', function() {
             const promise = this.client.exec();
+
             expect(promise).to.be.an.instanceof(Promise);
           });
 
           describe('and no items left in client queues', function() {
             it('should resolve undefined', function* () {
-              this.client.queues = generateQueue(0);
+              this.client.queues = this.generateQueue(0);
+
               const result = yield this.client.exec();
+
               expect(result).to.equal(undefined);
             });
           });
         }
 
-        describe('when client is busy', function() {
+        describe('and client is busy', function() {
           beforeEach(function() {
             this.client.bufferedResponse = 'pending';
           });
@@ -322,7 +355,7 @@ describe('VNDBClient', function() {
           testLazyExec();
         });
 
-        describe('when client has not connected yet', function() {
+        describe('and client has not connected yet', function() {
           beforeEach(function() {
             this.client.socket = null;
           });
@@ -330,7 +363,7 @@ describe('VNDBClient', function() {
           testLazyExec();
         });
 
-        describe('when client is still connecting', function() {
+        describe('and client is still connecting', function() {
           beforeEach(function() {
             this.client.socket.connecting = true;
           });
@@ -339,45 +372,42 @@ describe('VNDBClient', function() {
         });
       });
 
-      describe('on handling client.write fulfillment', function() {
-        describe('on client.write promise resolved', function() {
-          beforeEach(function() {
-            this.client.write.resolves('write result');
-          });
+      describe('when handling resolved write', function() {
+        it('should pipe the result into the processed item\'s promise.resolve', function* () {
+          this.client.write.resolves('write result');
 
-          it('should pipe the result into the processed item\'s promise.resolve', function* () {
-            this.client.exec();
-            const result = yield this.itemToExec.promise;
+          const result = yield this.client.exec();
 
-            expect(result).to.equal('write result');
-          });
-
-          it('should call itself to process next message', function() {
-            this.sandbox.spy(this.client, 'exec');
-            this.client.exec().then(() => {
-              expect(this.client.exec).to.have.been.calledTwice;
-            });
-          });
+          expect(result).to.equal('write result');
         });
 
-        describe('on client.write promise rejected', function() {
-          beforeEach(function() {
-            this.client.write.rejects('something wrong');
-          });
+        it('should call itself to process next message', function* () {
+          this.client.write.resolves('write result');
+          this.sandbox.spy(this.client, 'exec');
 
-          it('should pipe the result into the processed item\'s promise.reject', function* () {
-            const error = yield this.catchError(this.client.exec());
+          yield this.client.exec();
 
-            expect(error).to.be.an.instanceof(Error)
-              .with.property('message', 'something wrong');
-          });
+          expect(this.client.exec).to.have.been.calledTwice;
+        });
+      });
 
-          it('should call itself to process next message', function() {
-            this.sandbox.spy(this.client, 'exec');
-            this.client.exec().catch(() => {
-              expect(this.client.exec).to.have.been.calledTwice;
-            });
-          });
+      describe('when handling rejected write', function() {
+        it('should pipe the result into the processed item\'s promise.reject', function* () {
+          this.client.write.rejects('something wrong');
+
+          const error = yield this.catchError(this.client.exec());
+
+          expect(error).to.be.an.instanceof(Error)
+            .with.property('message', 'something wrong');
+        });
+
+        it('should call itself to process next message', function* () {
+          this.client.write.rejects('something wrong');
+          this.sandbox.spy(this.client, 'exec');
+
+          yield this.catchError(this.client.exec());
+
+          expect(this.client.exec).to.have.been.calledTwice;
         });
       });
     });
@@ -580,6 +610,7 @@ describe('VNDBClient', function() {
     describe('when exec is successful', function() {
       it('should resolve the response as correct object', function* () {
         this.client.exec.resolves('dbstats {"users":1000,"vn":2000}');
+
         const result = yield this.client.dbstats();
 
         expect(result).to.deep.equal({
@@ -595,6 +626,7 @@ describe('VNDBClient', function() {
     describe('when exec is failed', function() {
       it('should rejects the error', function* () {
         this.client.exec.rejects(new VNDBError());
+
         const error = yield this.catchError(this.client.dbstats());
 
         expect(error).to.be.an.instanceof(VNDBError);
